477b938087f87b044575b91118c35c16a5f7b474
Crankshaft Robot <robot+crankshaft@gocardless.com>
2018-07-06 11:48:08 +0000

    v2.0.0

--- lib/Client.php
+++ lib/Client.php
@@ -52,7 +52,7 @@ class Client
                 'Content-Type' => 'application/json',
                 'Authorization' => "Bearer " . $access_token,
                 'GoCardless-Client-Library' => 'gocardless-pro-php',
-                'GoCardless-Client-Version' => '1.7.0',
+                'GoCardless-Client-Version' => '2.0.0',
                 'User-Agent' => $this->getUserAgent()
                 ),
                 'http_errors' => false,
@@ -317,7 +317,7 @@ class Client
     {
         $curlinfo = curl_version();
         $uagent = array();
-        $uagent[] = 'gocardless-pro-php/1.7.0';
+        $uagent[] = 'gocardless-pro-php/2.0.0';
         $uagent[] = 'schema-version/2015-07-06';
         $uagent[] = 'GuzzleHttp/' . \GuzzleHttp\Client::VERSION;
         $uagent[] = 'php/' . phpversion();
