e31a4a98fb6b20df12bf99e144b0f3a9ce273581
Robot <robot@gocardless.com>
2022-04-29 10:15:47 +0000

    Changes generated by 2d45403e4e9377d0362e15b678154584642d07b0

--- lib/Resources/BillingRequest.php
+++ lib/Resources/BillingRequest.php
@@ -84,6 +84,8 @@ class BillingRequest extends BaseResource
      * One of:
      * <ul>
      * <li>`pending`: the billing request is pending and can be used</li>
+     * <li>`authorised_with_bank`: the billing request is authorised with
+     * bank</li>
      * <li>`ready_to_fulfil`: the billing request is ready to fulfil</li>
      * <li>`fulfilled`: the billing request has been fulfilled and a payment
      * created</li>
