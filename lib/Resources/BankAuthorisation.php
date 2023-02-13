bc55324fc05f654afc7b7d9a21a9cbddd48c12f1
Robot <robot@gocardless.com>
2023-02-13 18:17:28 +0000

    Changes generated by fc759c636baddf84c2699962005ea0a03a1b2056

--- lib/Resources/BankAuthorisation.php
+++ lib/Resources/BankAuthorisation.php
@@ -78,6 +78,9 @@ class BankAuthorisation extends BaseResource
      * parameter for displaying the result of the
      * bank authorisation as outlined above.
      * 
+     * The BillingRequestFlow ID will also be appended to the `redirect_uri` as
+     * query parameter `id=BRF123`.
+     * 
      * Defaults to `https://pay.gocardless.com/billing/static/thankyou`.
      */
     protected $redirect_uri;
